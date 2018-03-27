18d6b31bb9a8f7d534dded83cf07ca578c291f92
xzjs <2310976219@qq.com>
2018-03-27 23:21:19 +0800

    Initial commit

---
+++ database/seeds/AdminTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AdminTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('admins')->insert([
+            'pwd' => md5("admin"),
+        ]);
+    }
+}
